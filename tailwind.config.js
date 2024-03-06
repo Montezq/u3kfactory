fac41aeac615d6d374c380100e654f22a6161b8e
Monte <intalmaci21@gmail.com>
2024-03-06 15:47:04 +0200

    addding fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      helvetica: ['"Helvetica Thin"', "sans-serif"],
+      helveticaThinCond: ['"Helvetica ThinCond"', "sans-serif"],
+      helveticaThinCondObl: ['"Helvetica ThinCondObl"', "sans-serif"],
+      helveticaThinItalic: ['"Helvetica ThinItalic"', "sans-serif"],
+      helveticaThinExtObl: ['"Helvetica ThinExtObl"', "sans-serif"],
+      helveticaThinExt: ['"Helvetica ThinExt"', "sans-serif"],
+      coutureBoldIt: ['"Couture BoldItalic"', "sans-serif"],
+      coutureBold: ['"Couture Bold"', "sans-serif"],
+      open: ['"Open Sans"', "sans-serif"],
+      axis: ['"AXIS Extra"', "sans-serif"]
+    },
   },
   plugins: [],
 }
