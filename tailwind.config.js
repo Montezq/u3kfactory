137c9e233d8bedb7fa2d32f4e57247a0ba26462b
Monte <intalmaci21@gmail.com>
2024-05-30 21:09:27 +0300

    working with cart

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       helveticaThinCond: ['"Helvetica Neue LT Std"', "sans-serif"],
       helveticaCond: ['"Helvetica Cond"', "sans-serif"],
       helvetica: ['"Neue Helvetica"', "sans-serif"],
+      helveticaLight: ['"Neue Helvetica Light"', "sans-serif"],
       coutureBoldIt: ['"Couture BoldItalic"', "sans-serif"],
       coutureBold: ['"Couture Bold"', "sans-serif"],
       open: ['"Open Sans"', "sans-serif"],
