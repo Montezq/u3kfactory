3e458ae05ae8dfb2a8dda259689a1a14655cb108
Ion Talmaci <iontalmaci4@gmail.com>
2024-12-19 13:33:56 +0200

    unavailable rules

--- assets/global.js
+++ assets/global.js
@@ -964,7 +964,7 @@ class VariantSelects extends HTMLElement {
     this.updateOptions();
     this.updateMasterId();
     this.updateSelectedSwatchValue(event);
-    this.toggleAddButton(true, '', false);
+    // this.toggleAddButton(true, '', false);
     this.updatePickupAvailability();
     this.removeErrorMessage();
     this.updateVariantStatuses();
@@ -976,15 +976,7 @@ class VariantSelects extends HTMLElement {
 
     // Check if the variant has changed
     if (previousVariantId !== currentVariantId) {
-      if ((!this.currentVariant || !this.currentVariant.available) && (collections.includes('prints') && (!collections.includes('emanuel') && !collections.includes('valen') && !collections.includes('mihu') && !collections.includes('gavriil')) )) {
-        const instructionsSent = document.querySelector('.combination-modal');
-        if (instructionsSent) {
-          instructionsSent.classList.remove('hidden');
-          setTimeout(() => {
-            instructionsSent.classList.add('hidden');
-          }, 2000); // Wait for 1 second before hiding the modal
-          history.replaceState(null, null, ' ');
-        }
+      if(!this.currentVariant){
         this.toggleAddButton(true, '', true);
         this.setUnavailable();
       } else {
@@ -1257,6 +1249,14 @@ class VariantSelects extends HTMLElement {
 
     if (!addButton) return;
     addButtonText.textContent = window.variantStrings.unavailable;
+    const instructionsSent = document.querySelector('.combination-modal');
+    if (instructionsSent) {
+      instructionsSent.classList.remove('hidden');
+      setTimeout(() => {
+        instructionsSent.classList.add('hidden');
+      }, 2000); // Wait for 1 second before hiding the modal
+      history.replaceState(null, null, ' ');
+    }
     if (price) price.classList.add('hidden');
     if (inventory) inventory.classList.add('hidden');
     if (sku) sku.classList.add('hidden');
